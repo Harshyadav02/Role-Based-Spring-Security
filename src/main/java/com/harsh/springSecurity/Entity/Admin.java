d6c4014530b24e0c7b0329fdb85e5de09f7442f0
harshyadav02 <harshyadav5736@gmail.com>
2024-08-27 17:40:43 +0530

    files holds the detail about admin user

---
+++ src/main/java/com/harsh/springSecurity/Entity/Admin.java
@@ -0,0 +1,17 @@
+package com.harsh.springSecurity.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Admin {
+    @Id 
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, length = 5)
+    private String roles = "ADMIN";
+}
