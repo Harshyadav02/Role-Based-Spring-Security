66bb8217a037d3fdf5ff2698ce945f3aa401fccc
harshyadav02 <harshyadav5736@gmail.com>
2024-08-28 08:29:33 +0530

    Added comment in get mapping

--- src/main/java/com/harsh/springSecurity/Controller/AdminController.java
+++ src/main/java/com/harsh/springSecurity/Controller/AdminController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +26,7 @@ public class AdminController {
     public ResponseEntity<?> getAdminDetails(){
 
         try {
+            // retriving admin details from basic auth
             String username = SecurityContextHolder.getContext().getAuthentication().getName();
             return new ResponseEntity<>(adminService.getAdminDetails(username),HttpStatus.OK);
         } catch (Exception e) {
