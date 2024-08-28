e9f16d4977af212b9fc6a27fe095179f222b6797
harshyadav02 <harshyadav5736@gmail.com>
2024-08-28 08:13:45 +0530

    added JsonIgnore for passwprd

--- src/main/java/com/harsh/springSecurity/Entity/Admin.java
+++ src/main/java/com/harsh/springSecurity/Entity/Admin.java
@@ -1,5 +1,7 @@
 package com.harsh.springSecurity.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -8,10 +10,11 @@ import lombok.Data;
 @Entity
 @Data
 public class Admin {
-    @Id 
+    @Id
     private String username;
+    @JsonIgnore
     @Column(nullable = false)
-    private String password;
+    private  String password;
     @Column(nullable = false, length = 5)
     private String roles = "ADMIN";
 }
