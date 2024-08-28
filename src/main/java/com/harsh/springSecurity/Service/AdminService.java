020b767b57871f8722de1c342bb0ceeb9029e2e8
harshyadav02 <harshyadav5736@gmail.com>
2024-08-28 08:15:22 +0530

    updated getAdminDetail to find admin

---
+++ src/main/java/com/harsh/springSecurity/Service/AdminService.java
@@ -0,0 +1,29 @@
+package com.harsh.springSecurity.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.harsh.springSecurity.Entity.Admin;
+import com.harsh.springSecurity.Repository.AdminRepo;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private AdminRepo adminRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    public void createAdminUser(Admin adminDetails) {
+        
+        // password encryption
+        adminDetails.setPassword(passwordEncoder.encode(adminDetails.getPassword()));
+        adminRepo.save(adminDetails);
+    }
+    public Admin getAdminDetails(String username) {
+        Admin adminDetails = adminRepo.findByUsername(username);
+        return adminDetails;
+    }
+    
+}
