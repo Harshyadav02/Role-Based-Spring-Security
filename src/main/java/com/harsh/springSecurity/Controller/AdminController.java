b6ced5fca3780af1322f623ad15c9fa1711aa603
harshyadav02 <harshyadav5736@gmail.com>
2024-08-28 08:12:37 +0530

    Added securityContexHolder to get admin detail

--- src/main/java/com/harsh/springSecurity/Controller/AdminController.java
+++ src/main/java/com/harsh/springSecurity/Controller/AdminController.java
@@ -3,6 +3,9 @@ package com.harsh.springSecurity.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,19 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+
+    @GetMapping()
+    
+    public ResponseEntity<?> getAdminDetails(){
+
+        try {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            return new ResponseEntity<>(adminService.getAdminDetails(username),HttpStatus.OK);
+        } catch (Exception e) {
+           
+            return new ResponseEntity<>(e.getLocalizedMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     //  Adding new Admin user
     @PostMapping()
     public ResponseEntity<?> createAdmin(@RequestBody Admin adminDetails){
