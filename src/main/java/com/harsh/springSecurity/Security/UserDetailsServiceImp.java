164e8e13c5e36b364776c16225fdfdda30924386
harshyadav02 <harshyadav5736@gmail.com>
2024-08-28 08:24:11 +0530

    Custom UserDetailService implemntation implementing UserDetailsService

--- src/main/java/com/harsh/springSecurity/Security/UserDetailsServiceImp.java
+++ src/main/java/com/harsh/springSecurity/Security/UserDetailsServiceImp.java
@@ -15,14 +15,14 @@ public class UserDetailsServiceImp implements UserDetailsService {
 
     @Autowired
     private AdminRepo adminRepo;
-    
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         Admin adminDetail = adminRepo.findByUsername(username);
 
         if (adminDetail == null) {
-            throw new UsernameNotFoundException("User with username\t" + username + "not found");
+            throw new UsernameNotFoundException("No such Admin with username\t" + username);
         }
 
         // building User Object by authecation provider and returing it to authentication manager
