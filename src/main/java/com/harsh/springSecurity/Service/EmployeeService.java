0a3d518785e1576f6d2bc659d7b1372c5b1cc387
harshyadav02 <harshyadav5736@gmail.com>
2024-08-29 09:17:56 +0530

    return type changed from employee to responseEntity in getEmployeeData method

--- src/main/java/com/harsh/springSecurity/Service/EmployeeService.java
+++ src/main/java/com/harsh/springSecurity/Service/EmployeeService.java
@@ -1,6 +1,8 @@
 package com.harsh.springSecurity.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +16,12 @@ public class EmployeeService {
     @Autowired 
     private EmployeeRepo employeeRepo;
 
-    public Employee getEmployeeData(String username){
+    public ResponseEntity<?> getEmployeeData(String email){
 
-        return employeeRepo.findByEmail(username);
+        Employee empDetails = employeeRepo.findByEmail(email);
+        if(empDetails!=null) return new ResponseEntity<>(empDetails,HttpStatus.OK);
+        return new ResponseEntity<>("No such user",HttpStatus.NOT_FOUND);
+        
     }
     public void createEmployee(Employee empDetails){
         String encryptedPassword = passwordEncoder.encode(empDetails.getPassword());
