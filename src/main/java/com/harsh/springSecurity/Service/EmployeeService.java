6c2c64035b5b4554a2936d182481e0f963d17933
harshyadav02 <harshyadav5736@gmail.com>
2024-08-29 08:37:50 +0530

    added username as parameter to getAllEmployee

---
+++ src/main/java/com/harsh/springSecurity/Service/EmployeeService.java
@@ -0,0 +1,26 @@
+package com.harsh.springSecurity.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.harsh.springSecurity.Entity.Employee;
+import com.harsh.springSecurity.Repository.EmployeeRepo;
+
+@Service
+public class EmployeeService {
+    @Autowired 
+    private PasswordEncoder passwordEncoder;
+    @Autowired 
+    private EmployeeRepo employeeRepo;
+
+    public Employee getAllEmployee(String username){
+
+        return employeeRepo.findByEmail(username);
+    }
+    public void createEmployee(Employee empDetails){
+        String encryptedPassword = passwordEncoder.encode(empDetails.getPassword());
+        empDetails.setPassword(encryptedPassword);
+        employeeRepo.save(empDetails);
+    }
+}
