e58dc09f9800b8cec778dc3dbc6ff046bc9081f7
harshyadav02 <harshyadav5736@gmail.com>
2024-09-01 08:49:54 +0530

    updateEmployee based on given data

--- src/main/java/com/harsh/springSecurity/Service/EmployeeService.java
+++ src/main/java/com/harsh/springSecurity/Service/EmployeeService.java
@@ -28,4 +28,36 @@ public class EmployeeService {
         empDetails.setPassword(encryptedPassword);
         employeeRepo.save(empDetails);
     }
+
+    public ResponseEntity<?> updateEmployeeData(String email,Employee empDetails) {
+        // Retrieve the existing employee from the database
+        Employee existingEmployee = employeeRepo.findByEmail(email);
+        if(existingEmployee==null) {
+            return new ResponseEntity<>("Not a valid user to perform update version",HttpStatus.FORBIDDEN);
+        }
+        // Update fields if they are not null
+        if (empDetails.getEmail() != null && !empDetails.getEmail().isEmpty()) {
+            existingEmployee.setEmail(empDetails.getEmail());
+        }
+        if (empDetails.getPassword() != null && !empDetails.getPassword().isEmpty()) {
+            existingEmployee.setPassword(empDetails.getPassword());
+        }
+        if (empDetails.getFirstName() != null && !empDetails.getFirstName().isEmpty()) {
+            existingEmployee.setFirstName(empDetails.getFirstName());
+        }
+        if (empDetails.getLastName() != null && !empDetails.getLastName().isEmpty()) {
+            existingEmployee.setLastName(empDetails.getLastName());
+        }
+        if (empDetails.getPhoneNumber() != null && !empDetails.getPhoneNumber().isEmpty()) {
+            existingEmployee.setPhoneNumber(empDetails.getPhoneNumber());
+        }
+        if (empDetails.getPosition() != null && !empDetails.getPosition().isEmpty()) {
+            existingEmployee.setPosition(empDetails.getPosition());
+        }
+    
+        
+        employeeRepo.save(existingEmployee);
+        return new ResponseEntity<>("Employee Updated", HttpStatus.OK);
+    }
+    
 }
